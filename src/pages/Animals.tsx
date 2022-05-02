b365bb1369ba92134660b80d1c3f27e4c1fa92fc
Youssef <youssef.as@gmail.com>
2022-05-02 18:08:29 +0200

    Added animals through Axios

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { IAnimal } from '../models/IAnimal'
+import { IAnimalResponse } from '../models/IAnimalResponse'
+import './../styles/animals.css'
 
 export const Animals = () => {
+  const [animals, setAnimals] = useState<IAnimal[]>([])
+
+  useEffect(() => {
+    if(animals.length !== 0) return;
+
+    axios.get('https://animals.azurewebsites.net/api/animals')
+    .then((res) => {
+      setAnimals(res.data);      
+    })
+  });
+
+
+
   return (
-    <div>Animals</div>
+    <>
+    <h1>Hello</h1>
+      {
+        animals.map((animal) => {
+          return(
+            <Link key={animal.id} to={"/animal/" + animal.id}>
+              <h5>{animal.latinName}</h5>
+              <img src={animal.imageUrl} />
+            </Link>
+          )
+        })
+      }
+    </>
   )
 }
